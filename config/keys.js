3c38f972a406eb4221a3b4ff4c597fb465ee3198
bilal05476 <ba4762313@gmail.com>
2021-02-18 17:28:50 +0500

    backend work

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://bilal:<password>@mern-shoppinglist.nrjtr.mongodb.net/mern-shoppinglist?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+//Item Model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+// @desc Get All Items
+// @access Public
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({date: -1})
+        .then(items => res.json(items))
+})
+
+// @route POST api/items
+// @desc Create Items
+// @access Public
+router.post('/', (req, res) => {
+    const newItem = new Item({name: req.body.name});
+    newItem.save().then(item => res.json(item));
+})
+
+// @route DELETE api/items
+// @desc Delete Items
+// @access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({success: true})))
+        .catch(err => res.status(404).json({success: false}))
+});
+
+
+module.exports =  router;
